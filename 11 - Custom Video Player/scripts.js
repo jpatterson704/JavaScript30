4c01230ee33c9aa9f8fa0388eaa500f55d5b2b37
John <jpatterson704@yahoo.com>
2021-10-04 11:09:34 +0300

    upto 15

--- 11 - Custom Video Player/scripts.js
+++ 11 - Custom Video Player/scripts.js
@@ -1,12 +1,13 @@
-const video = document.querySelector('.player__video');
+const video = document.querySelector('video.player__video');
 const playButton = document.querySelector('button.player__button.toggle');
 const volumeCtl = document.querySelector('input[name="volume"]');
 const speedCtl = document.querySelector('input[name="playbackRate"]');
 const backForwardButtons = document.querySelectorAll('button.player__button[data-skip]');
-const progressButton = document.querySelector('div.progress');
+const progressBar = document.querySelector('div.progress');
+const progressBarFill = document.querySelector('div.progress__filled');
+let holdMouse = false;
 
-playButton.addEventListener('click', (e)=>{
-    console.log(e,video.paused);
+function togglePlay(){
     if(video.paused){
         video.play();
         playButton.innerHTML='⏸'
@@ -15,26 +16,55 @@ playButton.addEventListener('click', (e)=>{
         video.pause();
         playButton.innerHTML='►'
     }
-       
-})
+}
 
-volumeCtl.addEventListener( 'input', (e)=>{
+function updateProgressBar(){
+    let fillPer = (video.currentTime/video.duration)*100;
+    progressBarFill.setAttribute("style", `flex-basis: ${fillPer}%;`);
+}
+
+function updateVolume(e){
     video.volume= e.target.value ;
-});
+}
+
+playButton.addEventListener('click',togglePlay);
+video.addEventListener('click',togglePlay);
+video.addEventListener ('timeupdate' ,updateProgressBar);
+
+volumeCtl.addEventListener( 'input',updateVolume);
 
 speedCtl.addEventListener( 'input', (e)=>{
     video.playbackRate = e.target.value ;
 });
 
 backForwardButtons.forEach( (button)=>{
-    console.log(video.currentTime)
     button.addEventListener( 'click', (e)=>{
         // video.curren
         video.currentTime+= +button.dataset.skip
-        console.log(video.currentTime, button.dataset.skip)
     })
 })
 
-progressButton.addEventListener( 'click',  e =>{
-    console.log((e.offsetX))
+progressBar.addEventListener( 'click',  e =>{
+    video.currentTime= (e.offsetX*video.duration)/progressBar.offsetWidth;
+    updateProgressBar();
 })
+
+progressBar.addEventListener('mousedown',(e)=>{
+    holdMouse= true;
+    console.log('mousedown', e.offsetX)
+    video.currentTime= (e.offsetX*video.duration)/progressBar.offsetWidth;
+});
+progressBar.addEventListener('mouseup',(e)=>{
+    console.log('mouseup');
+    holdMouse= false;
+    video.currentTime= (e.offsetX*video.duration)/progressBar.offsetWidth;
+    updateProgressBar();
+});
+progressBar.addEventListener('mousemove',(e)=>{
+    if(holdMouse){
+        video.currentTime= (e.offsetX*video.duration)/progressBar.offsetWidth;
+        updateProgressBar();
+    }
+ 
+});
+
